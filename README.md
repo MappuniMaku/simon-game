f004c2688e583d6f859ec6f3c76fef1f3d7b4d6a
Grigory Telenok <telenok.grigory@gmail.com>
2020-08-21 16:05:07 +1000

    Added basic functionality for game start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simon-game
+
+Github Pages: https://mappunimaku.github.io/simon-game/
--- index.js
+++ index.js
@@ -3,39 +3,64 @@ Vue.config.devtools = true;
 let vm = new Vue({
     el: '#simon-game',
     data: {
-
+        round: 0,
+        gameInProgress: false,
+        difficultyLevel: 'easy',
+        sequenceArray: [],
     },
     methods: {
-        playSound: function (e) {
-            let button = e.target;
-            if (!button.classList.contains("disabled")) {
-                let audio1 = new Audio('sounds/1.mp3');
-                let audio2 = new Audio('sounds/2.mp3');
-                let audio3 = new Audio('sounds/3.mp3');
-                let audio4 = new Audio('sounds/4.mp3');
+        startGame: function () {
+            this.gameInProgress = true;
+            this.sequenceArray.push(this.getRandomInteger());
+            this.showSequence();
+        },
+        clickHandler: function (e) {
+            let buttonElement = e.target;
+            if (!buttonElement.classList.contains('disabled')) {
+                let buttonNumber = buttonElement.dataset.buttonNumber;
+                this.playSound(buttonNumber);
 
-                let buttonNumber = button.dataset.buttonNumber;
-                switch (buttonNumber) {
-                    case '1':
-                        audio1.play();
-                        break;
-                    case '2':
-                        audio2.play();
-                        break;
-                    case '3':
-                        audio3.play();
-                        break;
-                    case '4':
-                        audio4.play();
-                        break;
-                    default:
-                        alert("Произошла ошибка!");
-                        console.log("Произошла ошибка! Клик зарегистрирован на:");
-                        console.log(e.target);
-                };
-                    
             }
-        }
+        },
+        showSequence: function () {
+            this.sequenceArray.forEach( (elem, index) => {
+                this.highlightButton(elem);
+                this.playSound(elem.toString());
+            });
+        },
+        highlightButton: function (buttonNumber) {
+            let buttonElement = document.querySelector(`.game__button[data-button-number="${buttonNumber}"]`);
+            buttonElement.classList.add("game__button_highlighted");
+            setTimeout(() => { buttonElement.classList.remove("game__button_highlighted") }, 300);
+        },
+        playSound: function (buttonNumber) {
+            let audio1 = new Audio('sounds/1.mp3');
+            let audio2 = new Audio('sounds/2.mp3');
+            let audio3 = new Audio('sounds/3.mp3');
+            let audio4 = new Audio('sounds/4.mp3');
+
+            switch (buttonNumber) {
+                case '1':
+                    audio1.play();
+                    break;
+                case '2':
+                    audio2.play();
+                    break;
+                case '3':
+                    audio3.play();
+                    break;
+                case '4':
+                    audio4.play();
+                    break;
+                default:
+                    alert('Произошла ошибка!');
+                    console.log('Произошла ошибка!');
+            };
+        },
+        getRandomInteger: function () { // работает в диапазоне от 1 до 4 включительно
+            let random = 1 + Math.random() * 4;
+            return Math.floor(random);
+        },
     },
     computed: {
         
